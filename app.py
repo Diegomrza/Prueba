e04d918f0cfeaaffcbc865926cef9a0b7341718d
Diegomrza <2993323220101@ingenieria.usac.edu.gt>
2020-10-24 23:54:36 -0600

    JUJU
    JUJU

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+#CORS(app)
 
 @app.route('/login', methods=['GET'])
 def login():
 
     if request.method == "GET":
 
-        return '<h1>Hola amigos de youtube</h1>'
+        return "Hola amigos de youtube"
 
 @app.route('/')
 def index():    
-	return "<h1>Texas</h1>"
+	return "Texas"
 
 if __name__ == "__main__":
 	app.run(threaded = True,port = 8000, debug = True)
